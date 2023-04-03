f33c4aa28a695f905ba61829b45e3dad47429dd2
aditithakur16 <104360657+aditithakur16@users.noreply.github.com>
2023-04-03 22:14:35 +0530

    Day-1

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Day-one from './Day-one';
 
 test('renders learn react link', () => {
   render(<App />);
---
+++ src/Day-one.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Day-one() {
+    return (
+      <div className="App">
+        <h1>Day-1</h1>
+      </div>
+    );
+  }
+  
+  export default Day-one;
+
+
