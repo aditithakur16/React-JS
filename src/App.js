b6ab2c7573961943a4ad92c336f5436c58cfb0bc
aditithakur16 <104360657+aditithakur16@users.noreply.github.com>
2023-04-09 22:45:48 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-
+import React from 'react';
 import './App.css';
-
-function App() {
+import ClassComponent from './ClassComponent';
+import BlogCard from './BlogCard';
+function App() { 
+  const blogArr = [
+    {
+      id: 1,
+      title: "Blog Title 1",
+      description: "ifjefjoiejowgjlwjg"
+    },
+    {
+      id: 2,
+      title: "Blog Title 2",
+      description: "ifjefjoiejowgjlwjg"
+    },
+    {
+      id: 3,
+      title: "Blog Title 3",
+      description: "ifjefjoiejowgjlwjg"
+    }
+  ]
   return (
     <div className="App">
       <h1>Hello React👋</h1>
+   //   <ClassComponent/>
+      const blogCard = blogArr.map((item, pos) => {
+        return (
+          <BlogCard/>
+        )
+      })
+      
     </div>
   );
 }
---
+++ src/ClassComponent.js
@@ -0,0 +1,19 @@
+//import React from 'react'
+
+// class ClassComponent extends React.Component {
+//     constructor()
+//     {
+
+//     }
+//     render()
+//     {
+//         return (
+//             <div>
+//                 <h1>ClassComponent</h1>
+//             </div>
+//           )
+//     }
+  
+// }
+
+//export default ClassComponent
